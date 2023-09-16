e26beeac103607a17ea79cc5db5e3c2ad8dbfd16
Lưu Gia Phong <luuphong016@gmail.com>
2023-09-16 13:49:12 +0700

    Update DB script

--- BirdFarmShop_DB_Script.sql
+++ BirdFarmShop_DB_Script.sql
@@ -39,7 +39,7 @@ CREATE TABLE [Bird]
 	[achievement] NVARCHAR(200),
 	[reproduction_history] SMALLINT,
 	[price] INT,
-	[description] NVARCHAR(200),
+	[description] NVARCHAR(MAX),
 	[dad_bird_id] VARCHAR(10),
 	[mom_bird_id] VARCHAR(10),
 	[discount] SMALLINT,
@@ -57,7 +57,7 @@ CREATE TABLE [Accessory]
 	[accessory_name] NVARCHAR(30),
 	[unit_price] INT,
 	[stock_quantity] SMALLINT,
-	[description] NVARCHAR(200),
+	[description] NVARCHAR(MAX),
 	[discount] SMALLINT,
 	[status] NVARCHAR(20),
 	CONSTRAINT PK_Accessory PRIMARY KEY ([accessory_id])
@@ -153,35 +153,52 @@ GO
 
 INSERT INTO [Bird] ([bird_id], [bird_name], [color], [age], [grown_age], [gender], [breed], [achievement], [reproduction_history], [price], [description], [dad_bird_id], [mom_bird_id], [discount], [status]) 
 VALUES 
-	('XT001', 'Vẹt Xích Thái', 'Xanh lá,vàng', 10, 36, 1, 'Vẹt Châu Á', 'Giải 3 Cuộc Thi Vẹt Châu Á Schauwerbung 2023', 
+	('XT001', N'Vẹt Xích Thái', N'Xanh lá,vàng', 10, 36, 1, N'Vẹt Châu Á', N'Giải 3 Cuộc Thi Vẹt Châu Á Schauwerbung 2023', 
 	0, 6500000, 
-	'Xét ở góc độ sinh học, Xích Thái (Vẹt Má Vàng) thuộc nhóm vẹt rừng đuôi dài có viền cổ – Ringneck/Long-tail Parakeets, với 2 đặc trưng của loài: chim trống trưởng thành có đường viền sẫm màu phía sau gáy (ring) và các cá thể chim đều có đuôi rất dài, chiếm khoảng 1/2 tổng chiều dài toàn thân. Xét ở góc độ vật nuôi – Alexandrine Parakeet được coi là loài vẹt khá thân thiện, dễ thuần, sức đề kháng khá tốt, với tuổi thọ có thể đạt đến 30-40 năm.
+	N'Xét ở góc độ sinh học, Xích Thái (Vẹt Má Vàng) thuộc nhóm vẹt rừng đuôi dài có viền cổ – Ringneck/Long-tail Parakeets, với 2 đặc trưng của loài: chim trống trưởng thành có đường viền sẫm màu phía sau gáy (ring) và các cá thể chim đều có đuôi rất dài, chiếm khoảng 1/2 tổng chiều dài toàn thân. Xét ở góc độ vật nuôi – Alexandrine Parakeet được coi là loài vẹt khá thân thiện, dễ thuần, sức đề kháng khá tốt, với tuổi thọ có thể đạt đến 30-40 năm.
 	Được đánh giá là loài vẹt có khả năng nhại tốt, thực tế chất lượng nhại giọng nói của Vẹt má vàng thua xa chim Nhồng hoặc vẹt xám Châu Phi. Chim bắt chước giọng người không giỏi lắm, đặc biệt khó bắt chước các từ nhiều nguyên âm o, a. Ghi nhận trên thế giới, chỉ một số ít cá thể có thể nhớ và học được đến khoảng 20 từ.',
 	NULL, NULL, 0, 'available'),
 
-	('IR001', 'Indian Ringneck', 'Xanh dương', 16, 18, 1, 'Vẹt Châu Á', NULL, 
+	('IR001', 'Indian Ringneck', N'Xanh dương', 16, 18, 1, N'Vẹt Châu Á', NULL, 
 	1, 9000000, 
-	'Indian Ringneck hay còn gọi là Rose-ringed Parakeets thực sự là những sinh vật kỳ diệu. Thực vậy, chỉ cần nhìn vào những con chim bạn có thể dễ dàng nhận ra chúng qua màu sắc, cái mỏ nổi và long đuôi dài. Cả chim trống và mái đều có đuôi lớn và dài bao gồm 12 lông, 2 lông đuôi lớn chím phần lớn kích thước của con vẹt. Về màu sắc chúng có những màu phổ biến như xanh lá, trắng, vàng, xanh dương. Một số dòng đặc biệt có các màu sắc khá đẹp như xám khói, tím, xanh pastel… Con trống và mái có vẻ ngoài rất giống nhau tuy nhiên những con vẹt trống khi trưởng thành có thể phân biệt bằng vòng đen, trắng hoặc hồng quanh cổ.
+	N'Indian Ringneck hay còn gọi là Rose-ringed Parakeets thực sự là những sinh vật kỳ diệu. Thực vậy, chỉ cần nhìn vào những con chim bạn có thể dễ dàng nhận ra chúng qua màu sắc, cái mỏ nổi và long đuôi dài. Cả chim trống và mái đều có đuôi lớn và dài bao gồm 12 lông, 2 lông đuôi lớn chím phần lớn kích thước của con vẹt. Về màu sắc chúng có những màu phổ biến như xanh lá, trắng, vàng, xanh dương. Một số dòng đặc biệt có các màu sắc khá đẹp như xám khói, tím, xanh pastel… Con trống và mái có vẻ ngoài rất giống nhau tuy nhiên những con vẹt trống khi trưởng thành có thể phân biệt bằng vòng đen, trắng hoặc hồng quanh cổ.
 	Ringnecks có nguồn gốc từ Châu Á và Châu Phi và có thể được tìm thấy trong các khu rừng hoặc môi trường khô cằn và cũng xa lạ khi có thể thấy chúng tại các khu đô thị như ở California, Florida và cả vương quốc Anh.
 	Giống như hầu hết các loài vẹt, ringneck thông minh và làm vật nuôi tốt. Chúng học hỏi khá nhanh và thích thể hiện. Ngoài ra chúng còn có thể nói và phát âm khá rõ, một số con nói khá tốt như các loài vẹt Xám Châu Phi hay Amazon tuy khả năng ghi nhớ về số lượng từ vựng không tốt bằng.',
 	NULL, NULL, 5, 'available'),
 
-	('IR002', 'Indian Ringneck', 'Trắng', 10, 18, 0, 'Vẹt Châu Á', 'Giải 2 Cuộc Thi Vẹt Châu Á Australasia 2022', 
+	('IR002', 'Indian Ringneck', N'Trắng', 10, 18, 0, N'Vẹt Châu Á', N'Giải 2 Cuộc Thi Vẹt Châu Á Australasia 2022', 
 	0, 9000000, 
-	'Indian Ringneck hay còn gọi là Rose-ringed Parakeets thực sự là những sinh vật kỳ diệu. Thực vậy, chỉ cần nhìn vào những con chim bạn có thể dễ dàng nhận ra chúng qua màu sắc, cái mỏ nổi và long đuôi dài. Cả chim trống và mái đều có đuôi lớn và dài bao gồm 12 lông, 2 lông đuôi lớn chím phần lớn kích thước của con vẹt. Về màu sắc chúng có những màu phổ biến như xanh lá, trắng, vàng, xanh dương. Một số dòng đặc biệt có các màu sắc khá đẹp như xám khói, tím, xanh pastel… Con trống và mái có vẻ ngoài rất giống nhau tuy nhiên những con vẹt trống khi trưởng thành có thể phân biệt bằng vòng đen, trắng hoặc hồng quanh cổ.
+	N'Indian Ringneck hay còn gọi là Rose-ringed Parakeets thực sự là những sinh vật kỳ diệu. Thực vậy, chỉ cần nhìn vào những con chim bạn có thể dễ dàng nhận ra chúng qua màu sắc, cái mỏ nổi và long đuôi dài. Cả chim trống và mái đều có đuôi lớn và dài bao gồm 12 lông, 2 lông đuôi lớn chím phần lớn kích thước của con vẹt. Về màu sắc chúng có những màu phổ biến như xanh lá, trắng, vàng, xanh dương. Một số dòng đặc biệt có các màu sắc khá đẹp như xám khói, tím, xanh pastel… Con trống và mái có vẻ ngoài rất giống nhau tuy nhiên những con vẹt trống khi trưởng thành có thể phân biệt bằng vòng đen, trắng hoặc hồng quanh cổ.
 	Ringnecks có nguồn gốc từ Châu Á và Châu Phi và có thể được tìm thấy trong các khu rừng hoặc môi trường khô cằn và cũng xa lạ khi có thể thấy chúng tại các khu đô thị như ở California, Florida và cả vương quốc Anh.
 	Giống như hầu hết các loài vẹt, ringneck thông minh và làm vật nuôi tốt. Chúng học hỏi khá nhanh và thích thể hiện. Ngoài ra chúng còn có thể nói và phát âm khá rõ, một số con nói khá tốt như các loài vẹt Xám Châu Phi hay Amazon tuy khả năng ghi nhớ về số lượng từ vựng không tốt bằng.',
 	NULL, NULL, 0, 'pairing'),
 
-	('XA001', 'Vẹt Xích Ấn', 'Xanh lá', 5, 18, 1, 'Vẹt Châu Á', NULL, 
+	('XA001', N'Vẹt Xích Ấn', N'Xanh lá', 5, 18, 1, N'Vẹt Châu Á', NULL, 
 	0, 15000000, 
-	'Vẹt xích Ấn – xích Nepal có nguồn gốc từ vùng đông nam châu Á, bao gồm Nepal, Ấn Độ, Pakistan, và Sri Lanka. Chúng thường sống trong các khu rừng, cánh đồng và vùng đồng cỏ.
+	N'Vẹt xích Ấn – xích Nepal có nguồn gốc từ vùng đông nam châu Á, bao gồm Nepal, Ấn Độ, Pakistan, và Sri Lanka. Chúng thường sống trong các khu rừng, cánh đồng và vùng đồng cỏ.
 	Vẹt xích Ấn có một cái mỏ lớn, màu đen và một đốm màu đỏ trên vai. Một đặc điểm nổi bật của chúng là vòng cổ màu đỏ tươi rực, tạo ra sự tương phản đẹp với bộ lông xanh sẫm. Chúng là những vẹt thông minh và thích thể hiện khả năng nói chuyện. Tuy nhiên, khả năng nói chuyện của từng cá thể có thể khác nhau.',
-	NULL, NULL, 0, 'pairing')
+	NULL, NULL, 0, N'pairing'),
+
+	('CP101', N'Vẹt xám', N'Xám', 2, 5, 0, N'Vẹt Châu Phi', N'Chưa có', 0, 
+	2500000, NULL, NULL, NULL, 15, N'Đang nuổi dưỡng'),
+
+    ('CP201', N'Vẹt Cape Parrot', N'Xám, xanh', 3, 4, 1, N'Vẹt Châu Phi', N'Chưa có', 0, 
+	3000000, NULL, NULL, NULL, 0, N'Đang nuôi dưỡng'),
+
+    ('CP301', N'Vẹt Senegal', N'Xanh lá, vàng', 3, 1, 0, N'Vẹt Châu Phi', N'Chưa có', 2, 
+	8000000, NULL, NULL, NULL, 0, N'Đang nuôi dưỡng'),
+
+    ('CP401', N'Vẹt cổ trắng', N'Xanh lá, đỏ', 2, 1, 0, N'Vẹt Châu Phi', N'Chưa có', 1, 
+	19000000, NULL, NULL, NULL, 0, N'Đang sinh sản'),
+
+    ('CP501', N'Vẹt Lovebird', N'Xanh, vàng, cam', 2, 1, 1, N'Vẹt Châu Phi', N'Chưa có', 1, 
+	2000000, NULL, NULL, NULL, 0, N'Đang sinh sản');
 	
 GO
 
 INSERT INTO [Accessory] (accessory_id, accessory_name, unit_price, stock_quantity, description, discount, status) 
 VALUES 
-	('accessory_id', 'accessory_name', unit_price, stock_quantity, 'description', discount, 'status');
+	('GT001', N'Găng tay bắt vẹt', 120000, 50, N'Dùng cho việc huấn luyện vẹt, tránh trầy xước hoặc bị đau', 0, 'Available'),
+	('GA001', N'Máy ghi âm', 600000, 25, N'Dùng cho việc huấn luyện vẹt nói', 0, 'Available'),
+	('CT001', N'Còi huấn luyện vẹt', 90000, 15, N'Huấn luyện trong việc nghe tín hiệu', 0, 'Available')
 
