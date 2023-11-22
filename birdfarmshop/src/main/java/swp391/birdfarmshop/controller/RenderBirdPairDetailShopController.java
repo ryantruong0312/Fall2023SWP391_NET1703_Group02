7370ac1cb488207a91df50e99762f9da1b1965e1
Ff2YR2dhwB <nhathoang1470@gmail.com>
2023-11-22 09:38:51 +0700

    edit RenderBirdPairDetailShopController.java, BirdDAO.java, bird-pair-detail.jsp

--- birdfarmshop/src/main/java/swp391/birdfarmshop/controller/RenderBirdPairDetailShopController.java
+++ birdfarmshop/src/main/java/swp391/birdfarmshop/controller/RenderBirdPairDetailShopController.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package swp391.birdfarmshop.controller;
 
 import java.io.IOException;
@@ -14,6 +13,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import java.util.ArrayList;
+import swp391.birdfarmshop.dao.BirdDAO;
 import swp391.birdfarmshop.dao.BirdPairDAO;
 import swp391.birdfarmshop.dao.TrackingBirdPairDAO;
 import swp391.birdfarmshop.dto.BirdPairDTO;
@@ -24,52 +24,63 @@ import swp391.birdfarmshop.model.User;
  *
  * @author Admin
  */
-@WebServlet(name="RenderBirdPairDetailShopController", urlPatterns={"/RenderBirdPairDetailShopController"})
+@WebServlet(name = "RenderBirdPairDetailShopController", urlPatterns = {"/RenderBirdPairDetailShopController"})
 public class RenderBirdPairDetailShopController extends HttpServlet {
-     private static final String ERROR = "RenderHomeController";
-     private static final String SUCCESS = "management/bird-pair-detail-shop.jsp";
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    private static final String ERROR = "RenderHomeController";
+    private static final String SUCCESS = "management/bird-pair-detail-shop.jsp";
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         String url = SUCCESS;
         try {
             HttpSession session = request.getSession();
             User u = (User) session.getAttribute("LOGIN_USER");
             String pair_id = request.getParameter("pair_id");
-            if(u != null){
-               if(!u.getRole().equals("customer")){
-                   BirdPairDAO bpd = new BirdPairDAO();
-                   TrackingBirdPairDAO trackingDao = new TrackingBirdPairDAO();
-                   BirdPairDTO birdPair = bpd.getBirdPairByBirdPairId(pair_id);
-                   ArrayList<TrackingDTO> trackingList = trackingDao.getTrackingBirdPair(birdPair.getPair_id());
-                   trackingList.size();
-                   request.setAttribute("BIRDPAIR", birdPair);
-                   request.setAttribute("TRACKINGLIST", trackingList);
-                   request.setAttribute("SIZE", trackingList.size());
-               }else{
-                   session.setAttribute("ERROR", "Bạn không có quyền truy cấp");
-               }
-            }else{
-                url=ERROR;
-                session.setAttribute("ERROR", "Bạn chưa đăng nhập"); 
+            if (u != null) {
+                if (!u.getRole().equals("customer")) {
+                    BirdPairDAO bpd = new BirdPairDAO();
+                    TrackingBirdPairDAO trackingDao = new TrackingBirdPairDAO();
+                    BirdPairDTO birdPair = bpd.getBirdPairByBirdPairId(pair_id);
+                    ArrayList<TrackingDTO> trackingList = trackingDao.getTrackingBirdPair(birdPair.getPair_id());
+                    trackingList.size();
+                    request.setAttribute("BIRDPAIR", birdPair);
+                    request.setAttribute("TRACKINGLIST", trackingList);
+                    request.setAttribute("SIZE", trackingList.size());
+                    if (birdPair.getStatus().equalsIgnoreCase("Đã sinh sản")) {
+                        BirdDAO birdDAO = new BirdDAO();
+                        boolean checkFemale_bird = birdDAO.updateReproductionHistory(birdPair.getFemale_bird().getBird_id(), birdPair.getFemale_bird().getReproduction_history());
+                        boolean checkMale_bird = birdDAO.updateReproductionHistory(birdPair.getMale_bird().getBird_id(), birdPair.getMale_bird().getReproduction_history());
+                        
+                    }
+                } else {
+                    session.setAttribute("ERROR", "Bạn không có quyền truy cấp");
+                }
+            } else {
+                url = ERROR;
+                session.setAttribute("ERROR", "Bạn chưa đăng nhập");                
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-        }finally{
+        } finally {
             request.getRequestDispatcher(url).forward(request, response);
         }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -77,12 +88,13 @@ public class RenderBirdPairDetailShopController extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -90,12 +102,13 @@ public class RenderBirdPairDetailShopController extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
